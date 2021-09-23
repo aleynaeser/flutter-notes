842de45e9cf51980b00f77c3ef7cc3a441667ec1
Aleyna Eser <45822686+Aleynaesr@users.noreply.github.com>
2021-09-23 22:48:13 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,128 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NickName Generator',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: RandomWords(),
+    );
+  }
+}
+
+class _RandomWordsState extends State<RandomWords> {
+
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  //SCROLL
+  Widget _buildSuggestions() {
+    return ListView.builder( //view words as list
+        padding: const EdgeInsets.all(16),
+
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider(); // divide list
+          }
+
+          final int index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
+
+  // add nice view to list
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red: null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+
+  Widget build(BuildContext context) {
+    return Scaffold (                     // Add from here...
+      appBar: AppBar(
+        title: const Text('NickName Generator'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved NickNames',
+              ),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+                (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved NickNames'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, //...to here.
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+
+
