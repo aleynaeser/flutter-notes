a80884bdf5caa3818ba20a0ff20db621ce37430d
Aleyna Eser <45822686+Aleynaesr@users.noreply.github.com>
2021-09-27 17:45:07 +0300

    Update secondVersion.dart

--- NickName Generator App/secondVersion.dart
+++ NickName Generator App/secondVersion.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
@@ -113,7 +113,16 @@ class AnaEkran extends State<HomeScreen> {
            return Card (
              child: Padding(
              padding: const EdgeInsets.all(20.0),
-                child: Text(mainDataList[index],style: TextStyle(fontSize: 19.0),),
+             child: SelectableText(mainDataList[index],
+                              cursorColor: Colors.purple,
+                              showCursor: true,
+                              toolbarOptions: ToolbarOptions(
+                              copy: true,
+                              selectAll: true,
+                              cut: false,
+                              paste: false
+                               ),
+                              style: TextStyle(fontSize: 19.0),),
 
              ),
 
